2d8a42d8c7f6a98ff12eefd7517145d50794609f
funsojoba <hrfunsojoba@gmail.com>
2025-12-13 06:34:00 +0100

    usd

--- src/pages/Donation/index.jsx
+++ src/pages/Donation/index.jsx
@@ -73,7 +73,7 @@ return (
                                     ariaLabel="loading"
                                 />
                             ) : (
-                                `$${amt}`
+                                `US$${amt}`
                             )}
                         </AmountButton>
                     ))}
